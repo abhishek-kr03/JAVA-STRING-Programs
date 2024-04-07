1e5305f5a9e3e37c2fdaf32d3e08cd3be773d86c
Abhishek Kumar <166202255+abhishek-kr03@users.noreply.github.com>
2024-04-07 13:07:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.stringprograms;
+
+import java.util.Scanner;
+
+//STRING REVERSE
+
+public class Main {
+	static String stringReverse(String s){
+		String t = "";
+		for (int i=s.length()-1 ; i>=0 ; i--) {
+			t = t + s.charAt(i);		
+		}
+		return t;
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the string value");
+		String s = scan.nextLine();
+		String res = stringReverse(s);
+		System.out.println(res);
+		scan.close();
+	}
+}
