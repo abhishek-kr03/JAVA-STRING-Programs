442f9e37262159c8fd2ddafdf1dc647f321a31f2
Abhishek Kumar <166202255+abhishek-kr03@users.noreply.github.com>
2024-04-07 13:11:13 +0530

    String Reverse
    Reversing the the given String literal

--- StringP1.java
+++ StringP1.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 	static String stringReverse(String s){
-		String t = "";
+		String t = "";	// initializing an empty string to store reverse string values.
 		for (int i=s.length()-1 ; i>=0 ; i--) {
 			t = t + s.charAt(i);		
 		}
